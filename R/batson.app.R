82625db0ed53e19a1fb43b95b9251123e4637bdb
Sachin S. Pandya <sachin.pandya@uconn.edu>
2021-07-09 15:29:04 -0400

    create package documentation page - initial

---
+++ R/batson.app.R
@@ -0,0 +1,11 @@
+#' batson.app.pkg: A Shiny app package for estimating bias in peremptory challenges
+#' 
+#' This package provides one function to run the Shiny app:
+#' run_app.
+#' 
+#' @section Foo functions:
+#' The foo functions ...
+#'
+#' @docType package
+#' @name batson.app.pkg
+NULL
